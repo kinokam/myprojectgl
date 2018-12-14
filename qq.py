6b8fefa7da3ff0783191491c048aa0928618a63a
kinokam <45312192+kinokam@users.noreply.github.com>
2018-12-14 22:49:11 +0300

    Проект Скрамбл
    Секундомер, багнутый генератор кода

--- qq.py
+++ qq.py
@@ -2,9 +2,8 @@ import random
 import sys
 import time
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QKeyEvent
 
 class SKRAMBL(QMainWindow):
     def __init__(self):
@@ -12,19 +11,23 @@ class SKRAMBL(QMainWindow):
         uic.loadUi('skrb.ui', self)
         self.timer1 = QTimer()
 
-        self.skrambl = ['F', 'F\'', 'F2',
-                   'B', 'B\'', 'B2',
-                   'U', 'U\'', 'U2',
-                   'D', 'D\'', 'D2',
-                   'R', 'R\'', 'R2',
-                   'L', 'L\'', 'L2']
+        self.btn.clicked.connect(self.tablo1)
 
-        self.n = random.randint(13, 25)
-        a = []
-        for i in range(self.n):
-            k = random.randint(0, len(self.skrambl) - 1)
-            a.append(self.skrambl[k])
-        sk = ('  '.join(a))
+
+        self.code = ['F', 'F\'', 'F2',
+                        'B', 'B\'', 'B2',
+                        'U', 'U\'', 'U2',
+                        'D', 'D\'', 'D2',
+                        'R', 'R\'', 'R2',
+                        'L', 'L\'', 'L2']
+
+        def skr_ambl(self):
+            n = random.randint(13, 25)
+            a = []
+            for i in range(n):
+                k = random.randint(0, len(self.code) - 1)
+                a.append(self.code[k])
+            self.sk = ('  '.join(a))
 
         self.fl = True
 
@@ -32,17 +35,22 @@ class SKRAMBL(QMainWindow):
 
 
     def tablo(self):
-        self.timer.setText(str(time.time() - self.t))
+        self.timer.setText(str(time.time() - self.time1))
+
+    def tablo1(self):
+        self.skrambl.setText(self.sk)
 
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space:
             if self.fl:
                 self.timer1.start(10)
-                self.t = time.time()
+                self.time1 = time.time()
             else:
                 self.timer1.stop()
             self.fl = not self.fl
+
+
 
 
 app = QApplication(sys.argv)
