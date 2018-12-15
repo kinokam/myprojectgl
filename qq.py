1934c5c7e08ce1f6f6306a6d86b863d056d1b153
ssdhnik <45427470+ssdhnik@users.noreply.github.com>
2018-12-15 16:51:43 +0300

    Дополненный проект
    Добавил строку с рекордом;

--- qq.py
+++ qq.py
@@ -5,15 +5,26 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtCore import Qt, QTimer
 
-class SKRAMBL(QMainWindow):
+
+class SKRAMBL (QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('skrb.ui', self)
         self.timer1 = QTimer()
 
+        try:
+            file = open('Рекорд.txt')
+        except IOError:
+            pass
+        else:
+            with open('Рекорд.txt', 'r') as f:
+                f.seek(0)
+                first_char = f.read(1)
+                if first_char:
+                    f.seek(0)
+                    self.set(float(f.read()))
 
         self.btn.clicked.connect(self.tablo1)
-
 
         self.code = ['F', 'F\'', 'F2',
                         'B', 'B\'', 'B2',
@@ -21,7 +32,6 @@ class SKRAMBL(QMainWindow):
                         'D', 'D\'', 'D2',
                         'R', 'R\'', 'R2',
                         'L', 'L\'', 'L2']
-
 
         n = random.randint(13, 25)
         a = []
@@ -32,12 +42,13 @@ class SKRAMBL(QMainWindow):
 
         self.fl = True
 
-        self.timer1.timeout.connect(self.tablo
+        self.timer1.timeout.connect(self.tablo)
 
+    def set(self, record):
+        self.best.setText('{0:.3f}'.format(record))
 
     def tablo(self):
-        self.timer.setText('{0:.2f}'.format(time.time() - self.time1))
-                 #поставь сдесь ^ количество знаков после запятой
+        self.timer.setText('{0:.3f}'.format(time.time() - self.time1))
 
     def tablo1(self):
         n = random.randint(13, 25)
@@ -57,6 +68,29 @@ class SKRAMBL(QMainWindow):
                 self.timer1.start(10)
                 self.time1 = time.time()
             else:
+                tm = time.time() - self.time1
+                try:
+                    file = open('Рекорд.txt')
+                except IOError:
+                    with open('Рекорд.txt', 'w') as f:
+                        f.write('{0:.3f}'.format(tm))
+                        self.set(tm)
+                else:
+                    with open('Рекорд.txt', 'r') as f:
+                        f.seek(0)
+                        first_char = f.read(1)
+                    if not first_char:
+                        with open('Рекорд.txt', 'w') as f:
+                            f.write('{0:.3f}'.format(tm))
+                            self.set(tm)
+                    else:
+                        with open('Рекорд.txt', 'r') as f:
+                            best = float(f.read())
+                        if best > tm:
+                            with open('Рекорд.txt', 'w') as f:
+                                f.seek(0)
+                                f.write('{0:.3f}'.format(tm))
+                                self.set(tm)
                 self.timer1.stop()
             self.fl = not self.fl
 
